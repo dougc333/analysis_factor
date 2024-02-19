710519f63c43457754bed261810603ec5cac14a3
doug chang <dougc@stanfordalumni.org>
2024-02-19 10:19:47 -0800

    add

--- tableau_data/advanced_visualizations/test_maps/make_country_test_tableau.py
+++ tableau_data/advanced_visualizations/test_maps/make_country_test_tableau.py
@@ -1,13 +1,21 @@
 from collections import defaultdict
-import csv
+
+import redis
 
 class ParseLocation:
   def __init__(self,fileName:str):
     self.fileName = fileName
-    self.country = defaultdict(int) 
+    self.country = defaultdict(int)
+    self.state = defaultdict(int)
+    self.city = defaultdict(int) 
+    self.uid={}
     self.parse()
     self.writecsv()
-  
+    self.connect_redis()
+  def connect_redis(self):
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True,username="default", password="admin", db=0)
+    r.set("foo","bar")
+    print(r.get("foo"))  
   
   def parse(self):
     with open(self.fileName) as fh:
@@ -20,14 +28,34 @@ class ParseLocation:
         line = fh.readline()
         if not line: 
           break
-        testCountry = line.split("\t")[3]
-        if "," in testCountry:
-          print("not country:",testCountry)
+        location = line.split("\t")[3]
+        uid = line.split("\t")[0]
+        if "," in location:
+          if location.count(",")==1:
+            print("1 comma in location")
+            print(" state:",location.split(",")[0])
+            print(" country:",location.split(",")[1])   
+            self.uid[uid] = [{"state":location.split(",")[0]},{"country":location.split(",")[1]}]
+            self.state[location.split(",")[0]] +=1
+            self.country[location.split(",")[1]] += 1  
+          elif location.count(",")==2:
+            print("2 comma in location")
+            print(" city:",location.split(",")[0])
+            print(" state:",location.split(",")[1])
+            print(" country:",location.split(",")[2])
+            self.uid[uid] = [{"city":location.split(",")[0]},{"state":location.split(",")[1]},{"country":location.split(",")[2]}]
+            self.city[location.split(",")[0]] += 1
+            self.state[location.split(",")[1]] +=1
+            self.country[location.split(",")[2]] += 1  
         else:
-          print('testCountry:',testCountry)
-          self.country[testCountry] += 1
-      #print(self.country)
-      #print(len(self.country))
+          print(f'uid:{uid} location is country:{location}')
+          self.uid[uid] = {"country":location}
+          self.country[location] += 1    
+    print(self.uid["1"])
+    print("num cities:",len(self.city))
+    print("num states:",len(self.state))
+    print("num countries:",len(self.country))
+    print(self.state)
     fh.close()
     
   def writecsv(self):
@@ -39,5 +67,5 @@ class ParseLocation:
     
     
 
-p = ParseLocation("./demographics.tsv")
+p = ParseLocation("/Users/dc/analysis_factor/data/demographics.tsv")
 
