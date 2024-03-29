bda766e6dfa0700f4a94bacb6aa2094587b81a81
doug chang <dougc@stanfordalumni.org>
2024-03-29 14:11:37 -0700

    scatter plot

--- workshop_effects/module2_scaling_of_dependent_and_independent_variables/centering_cra/scatter/src/components/Centering.tsx
+++ workshop_effects/module2_scaling_of_dependent_and_independent_variables/centering_cra/scatter/src/components/Centering.tsx
@@ -1,34 +1,29 @@
 import Grid from "@mui/material/Grid"
 import { ChartContainer, ScatterPlot } from "@mui/x-charts"
-import {useState,useEffect} from 'react'
+import {useState} from 'react'
 import useSWR from 'swr'
 import axios from "axios"
 
 const ENDPOINT = './mcs2000.json'
 
 type MCSPCS = {
-  ID: number,
-  MCS2000:number, 
-  PCS2000:number
+  id: number,
+  x:number, 
+  y:number
 }
 
-
+type jsonType = {
+  ID:string,
+  MCS2000:string,
+  PCS2000:string
+}
 
 const Centering = () => {
   const [graphData, setGraphData] = useState(Array<MCSPCS>)
-  //const [scatterData, setScatterData] = useState(Array<ScatterData>)
-
-  // function convertData(arg:Array<MCSPCS>): Array<ScatterData>{
-  //   console.log("convertData arg:",arg)
-  //   const foo = arg.map(data=>setScatterData([...scatterData,{id:parseInt(data["ID"])}])
-  //   )
-  //   console.log("convertData typeof foo:",(foo))
-  // }
 
   useSWR(ENDPOINT, key=>{
     console.log("key",key)
-    axios.get(key).then(r=>r.data).then(myData=>myData.map(data=> {return {id: parseInt(data["ID"]),x:parseInt(data["MCS2000"]),y:parseInt(data["PCS2000"])}})).then(myData=>setGraphData(myData))
-    
+    axios.get(key).then(r=>r.data).then(myData =>myData.map( (data:jsonType) => {return {id: parseInt(data["ID"]),x:parseInt(data["MCS2000"]),y:parseInt(data["PCS2000"])}})).then(myData=>setGraphData(myData))
   })
   console.log("graphdata",graphData)
   
